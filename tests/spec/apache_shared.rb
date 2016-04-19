161df8d3dd45ca12c7c4bb94c967e0f756fe3550
ryancurrah <Ryan.Currah@cantire.com>
2016-04-19 19:35:57 -0400

    added way more tests

--- tests/spec/apache_shared.rb
+++ tests/spec/apache_shared.rb
@@ -2,4 +2,32 @@ shared_examples 'apache::base' do
   describe package('httpd') do
     it { should be_installed }
   end
+
+  describe user('apache') do
+    it { should exist }
+    it { should have_uid 48 }
+    it { should have_login_shell '/sbin/nologin' }
+    it { should belong_to_group 'apache' }
+  end
+
+  describe group('apache') do
+    it { should exist }
+     it { should have_gid 48 }
+  end
+
+  describe service('httpd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe process('httpd') do
+    it { should be_running }
+    its(:user) { should eq 'root' }
+  end
+
+  describe file('/etc/httpd/conf/httpd.conf') do
+    it { should be_owned_by('root') }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
 end
